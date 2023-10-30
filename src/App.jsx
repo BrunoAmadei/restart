c27a0601a854bff14be12eef0a16d281e5c680da
BrunoAmadei <brunoamadei98@gmail.com>
2023-10-30 09:43:27 -0300

    Calculadora IMC
    Criando componente da lógica do cálculo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import { useState } from 'react'
+import CalculadoraIMC from "./components/CalculadoraIMC"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <h1>restart </h1>
+        <h1>Calculadora de IMC</h1>
+        <p>
+          O IMC é reconhecido como padrão internacional para avaliar o grau de sobrepeso e obesidade. É calculado dividindo o peso (em kg) pela altura ao quadrado (em metros).
+          (Fonte: <a href="https://centrodeobesidadeediabetes.org.br/tudo-sobre-obesidade/calculadora-de-imc/#:~:text=Compartilhar%3A,ao%20quadrado%20(em%20metros)." target="_blank" rel="noopener noreferrer">Centro de obesidade e diabetes</a>)
+        </p>
       </div>
+      <CalculadoraIMC />
     </>
   )
 }
---
+++ src/components/CalculadoraIMC.jsx
@@ -0,0 +1,13 @@
+
+function calculadoraIMC() {
+    return (
+      <div>
+        <p>
+          Aqui ficará a lógica para calcular o IMC.
+        </p>
+      </div>
+    )
+  }
+  
+  export default calculadoraIMC;
+  
